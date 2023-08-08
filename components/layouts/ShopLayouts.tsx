f811bd4cd9cb4423f0c1dee7f6cf9768873bbd67
Romeran Rodriguez <romeran14@gmail.com>
2023-08-07 20:26:28 -0700

    181

---
+++ components/layouts/ShopLayouts.tsx
@@ -0,0 +1,39 @@
+import Head from "next/head"
+import { FC, PropsWithChildren } from 'react'
+
+interface Props {
+    title: string;
+    pageDescription: string;
+    imageFullUrl?: string
+}
+
+export const ShopLayouts: FC<PropsWithChildren<Props>> = ({ children, title, pageDescription, imageFullUrl }) => {
+    return (
+        <>
+            <Head>
+                <title>{ title }</title>
+                <meta name="description" content={pageDescription} />
+                <meta name="og:title" content={title} />
+                <meta name="og:description" content={pageDescription} />
+                { 
+                  imageFullUrl && <meta name="og:image" content={imageFullUrl} />
+                }
+            </Head>
+            <nav>
+                {/**Navbar */}
+            </nav>
+            <main style={{
+                margin: '80px auto',
+                maxWidth: '1440px',
+                padding: '0px 30px'
+            }} >
+                {children}
+            </main>
+            <footer>
+
+            </footer>
+        </>
+    )
+}
+
+export default ShopLayouts
